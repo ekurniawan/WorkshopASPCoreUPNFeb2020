b1a1a2d7e929e73f4b8731ade4a8ec05f4808aa3
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-02-19 14:12:42 +0700

    menambahkan interface mahasiswa

---
+++ DAL/MahasiswaDAL.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using WorkshopASPCore21.Models;
+
+namespace WorkshopASPCore21.DAL
+{
+    public class MahasiswaDAL : IMahasiswa
+    {
+        public MahasiswaDAL()
+        {
+            
+        }
+
+        public void Delete(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Mahasiswa> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Mahasiswa GetById(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(Mahasiswa mhs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Mahasiswa mhs)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
